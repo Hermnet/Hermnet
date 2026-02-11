dfbbfede58122b3bcf60fe60fa18bcd021f5423f
Álvaro García López <alvarogarciaprof@gmail.com>
2026-02-11 20:36:44 +0100

    Clase message creada

--- backend/src/main/java/com/hermnet/api/model/Message.java
+++ backend/src/main/java/com/hermnet/api/model/Message.java
@@ -1,5 +1,37 @@
 package com.hermnet.api.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mailbox")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Message {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 64, nullable = false)
+    private String recipientId;
+
+    @Column(length = 64, nullable = false)
+    private String senderIdHash;
+
+    @Lob
+    @Column(nullable = false)
+    private byte[] encryptedImage;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
