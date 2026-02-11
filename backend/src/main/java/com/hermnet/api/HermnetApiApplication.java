5976017d77ec3f159b8137ac3d96092b1c4b35e2
Álvaro García López <alvarogarciaprof@gmail.com>
2026-02-11 16:33:14 +0100

    Feature: Backend Skeleton and Infrastructure (#15)
    * Se añade el pryoecto Spring-Boot generado
    
    * El docker levanta sin problemas
    
    * Properties listo y test de arranque pasado #3

---
+++ backend/src/main/java/com/hermnet/api/HermnetApiApplication.java
@@ -0,0 +1,13 @@
+package com.hermnet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermnetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermnetApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hermnet/api/HermnetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermnet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermnetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
